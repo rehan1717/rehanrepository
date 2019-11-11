73d59f5ea5b652996f4f6a3198bb451d8dc21940
rehan1717 <57316741+rehan1717@users.noreply.github.com>
2019-11-11 15:59:31 +0500

    Add files via upload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="rehan.blade.php">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
