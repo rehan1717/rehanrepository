5c4da3a583f4555c004931bf39723fab8028d3b9
rehan1717 <57316741+rehan1717@users.noreply.github.com>
2019-11-11 15:57:49 +0500

    Add files via upload

--- resources/views/rehan.blade.php
+++ resources/views/rehan.blade.php
@@ -1,91 +1,38 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<head >
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Rehan
-                </div>
-
-                
+<body style="background-color: rgb(28, 177, 127) ">
+    
+    <div class="container-fluid" style="background-color: gray;padding:30px; border:  5px solid black;text-align: center ; font-size: 45px ; ">
+    
+        <p>*** WELCOME TO MHS PHOTOGRAPHY ***</p>  
+    </div>
+    
+    <div class="container-fluid" >
+        <h2 style="text-align: left ">Book Your Events </h2>
+        <button type="button" class="btn  btn-info">Mehndi</button>
+        <button type="button" class="btn btn-warning">Barat</button>
+        <button type="button" class="btn btn-primary">Walima</button>
+        <button type="button" class="btn btn-success">Nikah</button> <br><br><br><br><br><br>
+        <h2 style="text-align: center " ><u>Events Information </u></h2></br><br><br><br><br>
+       <div class="row">
+            
+            <div class="col-sm-3"><h2>MEHNDI</h2> <p> We will make your MEHNDI<br> function perfect with our <br>professional photographers. </p></div>
+            <div class="col-sm-3"><h2>BARAT</h2><p> We will make your BARAT<br> function perfect with our <br>professional photographers. </p></div>
+            <div class="col-sm-3"><h2>WALIMA</h2><p> We will make your WALIMA<br> function perfect with our <br>professional photographers. </p></div>
+            <div class="col-sm-3"><h2>NIKAH</h2><p> We will make your NIKAH<br> function perfect with our <br>professional photographers. </p></div>
             </div>
-        </div>
-    </body>
+        
+</body>
+
+
 </html>
